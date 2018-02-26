1c28024ea3901652a997c1022743582af5f9a4c0
Wesley Fukui <wes.fukui@gmail.com>
2018-02-26 14:20:06 -0700

    First

---
+++ server/cardController.js
@@ -0,0 +1,56 @@
+const getCard = async (req, res) => {
+  const db = req.app.get('db')
+  await db.get_profile().then((response) => {
+    res.status(200).send(response)
+  })
+}
+
+const createCard = async (req, res) => {
+  const db = req.app.get('db')
+  const {
+    name,
+    uploadedFileCloudinayrUrl,
+  } = req.body
+  await db.create_profile([name, uploadedFileCloudinayrUrl]).then((data) => {
+    res.status(200).send(data)
+  }).catch((error) => {
+    console.log(error)
+    res.status(500).send(error)
+  })
+}
+
+const deleteCard = async (req, res) => {
+  const db = req.app.get('db')
+  const {
+    name,
+  } = req.params
+  await db.delete_profile([name]).then((data) => {
+    res.status(200).send(data)
+  }).catch((error) => {
+    console.log(error)
+    res.status(500).send(error)
+  })
+}
+
+const updateCard = async (req, res) => {
+  const db = req.app.get('db')
+  const {
+    profilename,
+    newName,
+  } = req.body
+
+  await db.update_profile([newName, profilename]).then((data) => {
+    res.status(200).send(data)
+    console.log(data)
+  }).catch((error) => {
+    console.log(error)
+    res.status(500).send(error)
+  })
+}
+
+module.exports = (app) => {
+  app.get('/api/card', getCard)
+  app.post('/api/postCard', createCard)
+  app.delete('/api/deleteCard/:name', deleteCard)
+  app.put('/api/updateCard', updateCard)
+}
---
+++ src/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { MuiThemeProvider, createMuiTheme, Reboot } from 'material-ui';
+import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App';
+import store from './store'
+
+const theme = createMuiTheme({
+  palette: {
+    type: 'light',
+    primary: {
+      light: '#3d688f',
+      main: '#003e61',
+      dark: '#001837',
+    },
+    secondary: {
+      light: '#9e9994',
+      main: '#706b66',
+      dark: '#45403c',
+    },
+    error: {
+      main: '#ff0000',
+    },
+    contrastThreshold: 3,
+    tonalOffset: 0.2,
+  },
+})
+
+ReactDOM.render(
+  <div>
+    <Reboot />
+    <Provider store={store} >
+      <BrowserRouter>
+        <MuiThemeProvider theme={theme}>
+          <App />
+        </MuiThemeProvider>
+      </BrowserRouter>
+    </Provider>
+  </div>
+  , document.getElementById('root'),
+);
