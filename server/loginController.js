b6fcb5ba0301afa52ad6ca1bc8ea761a5c079279
Wesley Fukui <wes.fukui@gmail.com>
2018-04-18 20:51:58 -0600

    card display work

---
+++ server/loginController.js
@@ -0,0 +1,25 @@
+const bcrypt = require('bcryptjs')
+
+const proLogin = async (req, res) => {
+  const db = req.app.get('db')
+  const { email, password } = req.body
+
+  await db.get_pro_password([email]).then((data) => {
+    bcrypt.compare(password, data[0].user_password).then((body) => {
+      if (body === true) {
+        req.session.pro.proLoggedIn = true
+        req.session.pro.userId = (data[0].id)
+        req.session.pro.userName = `${data[0].first_name}-${data[0].last_name}`
+        req.session.pro.email = (data[0].email)
+        req.session.pro.stripeId = data[0].stripe_id
+      }
+      res.status(200).send(req.session.pro)
+    })
+  }).catch((error) => {
+    res.status(500).send(error)
+  })
+}
+
+module.exports = (app) => {
+  app.post('/api/proLogin', proLogin)
+}
--- src/components/displayCard.js
+++ src/components/displayCard.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
 import { withStyles } from 'material-ui/styles';
-import Card, { CardActions, CardContent, CardMedia } from 'material-ui/Card';
+import Card, { CardContent, CardMedia, Paper } from 'material-ui/Card';
 import axios from 'axios';
 import Button from 'material-ui/Button';
 import Typography from 'material-ui/Typography';
@@ -23,19 +23,15 @@ const DisplayCard = ({
   const card = cardData && cardData.map((card, index) => (
     <div key={index} >
       <Card className={classes.card}>
-        <CardMedia
-          className={classes.media}
-          image={card.profilepic}
-          title="Contemplative Reptile"
-        />
+        {/* <Paper elevation={3} className={classes.paper}> */}
+        <CardMedia className={classes.media} image={card.profilepic} />
         <CardContent>
-          <Typography variant="headline" component="h2">
-            {card.profilename}
-          </Typography>
+          <Typography clasName={card.profilename} />
           <Button onClick={() => deleteProfile(card.profilename)} color="primary">
                 Delete
           </Button>
         </CardContent>
+        {/* </Paper> */}
       </Card>
     </div>
   ))
@@ -51,9 +47,30 @@ const DisplayCard = ({
 const styles = {
   card: {
     maxWidth: 345,
+    margin: '15px 0',
+    display: 'flex',
+    justifyContent: 'center',
   },
   media: {
     height: 200,
+  },
+  paper: {
+    width: '95%',
+    right: '2.5%',
+    '@media (min-width: 769px)': {
+      width: '100%',
+      right: '0%',
+      display: 'flex',
+      justifyContent: 'space-around',
+      alignItems: 'center',
+    },
+  },
+  image: {
+    height: '250px',
+    '@media (min-width: 769px)': {
+      width: '500px',
+      margin: '20px 0',
+    },
   },
 };
 
